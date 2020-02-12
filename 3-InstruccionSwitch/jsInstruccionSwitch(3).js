176123dd2e0278e1d8a1d8dc27f40504c0d80594
alumno <alumno@utnfra.com.ar>
2020-02-12 12:21:35 -0300

    trabajo

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,7 +5,28 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch (mesDelAño) {
+        case "Enero":
+        alert ("Este mes tiene 30 dias o mas")
+        break;
+        case "Febrero":
+        alert ("Este dia no tiene mas de 29 dias")
+        break;
+        case "Marzo":
+        case "Abril":
+        case "Mayo":
+        case "Junio":
+        case "Julio":
+        case "Agosto":
+        case "Septiembre":
+        case "Octubre":
+        case "Noviembre":
+        case "Diciembre":
+        alert ("Este mes tiene 30 dias o mas")
+        break;
+        default:
+        break;
+    }
 
 
 }//FIN DE LA FUNCIÓN
