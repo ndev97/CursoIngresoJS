c50b0f8ae0940d8e58bbde6f1c5346f54be44ecd
alumno <alumno@utnfra.com.ar>
2020-02-26 12:14:22 -0300

    sd

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,9 @@
 function mostrar()
 {
-  
-}
+  var nombre;
+  var localidad;
+
+  nombre = document.getElementById("nombre").value;
+  while (!isNaN) {
+      nombre = prompt("Ingrese un nombre valido");
+  }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,25 @@
 
 function mostrar()
 {
+    var ancho;
+    var largo;
+    var perimetro;
+
+    ancho = prompt("Valor del ANCHO del rectangulo");
+    ancho = parseInt(ancho);
+    while (isNaN(ancho) || ancho < 1) {
+        ancho = prompt("Ingrese un numero valido:");
+        ancho = parseInt(ancho);
+    }
+
+    largo = prompt("Valor del LARGO del rectangulo");
+    largo = parseInt(largo);
+    while (isNaN(largo) || largo < 1) {
+        largo = promt("Ingrese un numero valido:");
+        largo = parseInt(largo);
+    }
+    perimetro = 2 * (ancho + largo);
+    perimetro = parseInt(perimetro);
+    alert("El perimetro del rectangulo es " + perimetro);
 
 }
