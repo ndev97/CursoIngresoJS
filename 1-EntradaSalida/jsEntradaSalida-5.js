52b2c30ad373a5bbdc2adad01f591adfc7ac5409
ndev97 <ncerdan97@gmail.com>
2020-02-27 22:11:59 -0300

    asdasd

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,14 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+	var dato;
+    var dato2;
+    var dato3;
+
+    dato = document.getElementById("ElNombre").value; 
+    dato2 = document.getElementById("laEdad").value;
+    dato3 = "Usted se llama " + dato + ", y tiene " + dato2 + " años.";
+
+    alert(dato3);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,63 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var numero1;
+    var numero2;
+    var total;
+
+    numero1 = document.getElementById("numeroUno").value;
+    numero2 = document.getElementById("numeroDos").value;
+    
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+
+    total = numero1 + numero2;
+
+    alert ("la suma es " + total);
 }
 
 function restar()
 {
-	
+	numero1 = document.getElementById("numeroUno").value;
+    numero2 = document.getElementById("numeroDos").value;
+    
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+
+    total = numero1 - numero2;
+
+    alert ("la resta es de " + total);
 }
 
 function multiplicar()
 { 
-	
+	var numero51;
+    var numero6;
+    var total;
+
+    numero1 = document.getElementById("numeroUno").value;
+    numero2 = document.getElementById("numeroDos").value;
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+
+    total = numero1 * numero2;
+
+    alert("la multiplicación es de " + total);
 }
 
 function dividir()
 {
-	
+	var numero1;
+    var numero2;
+    var total;
+
+    numero1 = document.getElementById("numeroUno").value;
+    numero2 = document.getElementById("numeroDos").value;
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+
+    total = numero1 / numero2;
+
+    alert("la división es de " + total);
 }
 
