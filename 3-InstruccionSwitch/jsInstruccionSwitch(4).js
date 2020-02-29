fbcfebc60e5079ce6c404732a3143a4dd1e9a463
ndev97 <ncerdan97@gmail.com>
2020-02-28 21:02:36 -0300

    asdasd

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,7 +4,27 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+	switch (mesDelAño) {
+        case "Enero":
+        case "Marzo":
+        case "Mayo":
+        case "Julio":
+        case "Agosto":
+        case "Octubre":
+        case "Diciembre":
+            alert("Este mes tiene 31 dias.");
+            break;
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+            alert("Este mes tiene 30 dias.");
+            break;
+        default:
+            alert("Este mes tiene 29 dias.");
+            break;
+        
+    }
 	
 
 
