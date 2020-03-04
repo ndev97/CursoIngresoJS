eb0588463806868295cdf244a3ec7aa207b93032
ndev97 <ncerdan97@gmail.com>
2020-03-03 21:33:47 -0300

    asdsa

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -6,15 +6,15 @@ function mostrar()
     var cantidadVarones;
     var sexo;
     var contador = 0;
-    var contadorNotas = 0;
     var acumuladorNotas = 0;
+    var sumaNotas;
     
     
     while (contador < 5) {
-        nota = prompt("Ingrese una NOTA");
+        nota = prompt("Ingrese una nota");
         nota = parseInt(nota);
 
-        while(isNaN(nota) || nota < 1 || nota > 10) {
+        while(isNaN(nota) && (nota < 1 || nota > 10)) {
             nota = prompt("Ingrese una nota valida");
             nota = parseInt(nota);
         }
@@ -23,6 +23,14 @@ function mostrar()
             sexo = prompt("Ingrese F o M");
         }
         contador++;
+        acumuladorNotas++;
     }
+    //punto A:
+    /*if(acumuladorNotas == 5) {
+        alert("El acumulador de notas es " + acumuladorNotas);
+    }*/
+    sumaNotas += nota;
+    sumaNotas = parseInt(sumaNotas);
+    alert("La suma de notas es " + sumaNotas);
 
 }
